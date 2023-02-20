c6095a55a9f4418fbdf4ecb3a53917544c7df1de
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 11:59:28 +0530

    Create my_script.py

---
+++ my_script.py
@@ -0,0 +1,15 @@
+import sys
+import openpyxl
+
+# Get the input Excel file path from the command-line arguments
+input_file = sys.argv[1]
+
+# Load the Excel file
+workbook = openpyxl.load_workbook(input_file)
+
+# Get the active worksheet
+worksheet = workbook.active
+
+# Get the value of cell A1 and print it
+cell_value = worksheet['A1'].value
+print(f"Input value is: {cell_value}")
